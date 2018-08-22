af86af03c9bd4a0483711dd9e8569442d77e9631
Can dang <candd1707@gmail.com>
2018-08-22 10:25:53 +0700

    update

--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -25,7 +25,7 @@ const collectibles = new Collectibles()
 // router.get("/docs", express.static(path.join(__dirname, "/src/Documents/help.html")))
 router.get("/docs",function(req, res){
     // res.send('Admin Homepage');
-    res.sendfile(path.join(__dirname, '../../docs/help.html'));
+    res.sendfile(path.join(__dirname, '../../help.html'));
 })
 
 router.get("/dapps/main", dAppsController.main);
